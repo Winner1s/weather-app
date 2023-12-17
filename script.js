429fef54595856b19076d174d043a19626a6684d
code commander <stefhob@outlook.com>
2023-12-17 09:34:05 -0500

    Code is working almost complete, need to add readme and adjust styling and include time in header area.

--- script.js
+++ script.js
@@ -1,20 +1,64 @@
-
-
-function GetAPI() {
-    const newName = document.getElementById("inputcity");
+document.addEventListener("DOMContentLoaded", function() {
+  function GetAPI() {
+    const newName = document.getElementById("inputcity").value;
     const cityName = document.getElementById("cityname");
-    cityName.innerHTML = "---" + newName.value + "--";
-  
-    fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + newName.value + '&appid=eef815747e53f2d4940ca279d8b75b2')
-      .then(response => response.json())
-      .then(data => {
-        // Process the weather data
-        for(i=0; i<5; i++)
-            document.getElementById("day" +(i + 1) + "Min").innerHTML = "Min" + Number(data.list[i].main.temp_min
-        -285.53).toFixed(1) + "0"
+    cityName.innerHTML = "---" + newName + "--";
+
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${newName}&appid=e19062db83c3ae61e40d3c134921009c`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
       })
-      .catch(error => {
-        console.log('Error:', error);
+      .then((data) => {
+        console.log(data); // Log the data object to the console for inspection
+
+        // Get the current date
+        const currentDate = new Date();
+
+        // Display current date
+        document.getElementById("currentDate").innerHTML = currentDate.toDateString();
+
+        // Process the weather data for future days
+        for (let i = 1; i <= 5; i++) {
+          const temps = [];
+          const descriptions = [];
+          const humidities = [];
+          const windSpeeds = [];
+
+          // Calculate average, minimum, and maximum temperatures, humidity, and wind speed for the day
+          for (let j = 0; j < 8; j++) {
+            temps.push(data.list[(i - 1) * 8 + j].main.temp - 273.15);
+            descriptions.push(data.list[(i - 1) * 8 + j].weather[0].description);
+            humidities.push(data.list[(i - 1) * 8 + j].main.humidity);
+            windSpeeds.push(data.list[(i - 1) * 8 + j].wind.speed);
+          }
+          const averageTemp = temps.reduce((sum, temp) => sum + temp, 0) / temps.length;
+          const minTemp = Math.min(...temps);
+          const maxTemp = Math.max(...temps);
+          const averageHumidity = humidities.reduce((sum, humidity) => sum + humidity, 0) / humidities.length;
+          const averageWindSpeed = windSpeeds.reduce((sum, windSpeed) => sum + windSpeed, 0) / windSpeeds.length;
+
+          // Get the date for the day
+          const day = new Date(currentDate);
+          day.setDate(currentDate.getDate() + i);
+
+          // Display date, average temperature, minimum temperature, maximum temperature, humidity, wind speed, and weather description for the day
+          const dayElement = document.getElementById("day" + i + "date");
+          dayElement.innerHTML = day.toDateString().substring(4);
+          dayElement.style.fontWeight = "extra-bold";
+          document.getElementById("day" + i + "date").innerHTML = day.toDateString().substring(4);
+          document.getElementById("day" + i + "temp").innerHTML = " " + averageTemp.toFixed(1) + "°c";
+          document.getElementById("day" + i + "min").innerHTML = "Min Temp: " + minTemp.toFixed(1) + "°c";
+          document.getElementById("day" + i + "max").innerHTML = "Max Temp: " + maxTemp.toFixed(1) + "°Fc";
+          document.getElementById("day" + i + "humidity").innerHTML = "Humidity: " + averageHumidity.toFixed(1) + "%";
+          document.getElementById("day" + i + "windspeed").innerHTML = "Wind Speed: " + averageWindSpeed.toFixed(1) + " m/s";
+          document.getElementById("day" + i + "desc").innerHTML = " " + descriptions[0];
+          document.getElementById("img" + i).src = "https://openweathermap.org/img/wn/" + data.list[(i - 1) * 8].weather[0].icon + ".png";
+        }
       });
   }
-  
+
+  document.getElementById("searchBtn").addEventListener("click", GetAPI);
+});
